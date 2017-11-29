f9db8a5bffc996888af863bbc2cfb05aa87378c4
Macbook Pro <macbookpro@MacBook-Pro-de-Macbook.local>
2017-11-29 17:08:06 -0600

    change solution fibonacci

--- src/ExerciseFibonacci.kt
+++ src/ExerciseFibonacci.kt
@@ -6,18 +6,14 @@ fun main(args: Array<String>){
 /**
  * fun fibonacci(last: Int = 0, current: Int = 1)*
  */
-fun fibonacci(last: Int = 0, current: Int = 1){
+fun fibonacci(last: Int = 0, current : Int = 1) {
 
-    if(last == 0){
-        println("${last} + ${last}  = ${(last + last)}")
-        println("${last} + ${current}  = ${(last + current)}")
-        fibonacci(current,last + current)
-    }else{
-        if(current < 30){
-            println("${last} + ${current}  = ${(last + current)}")
-            fibonacci(current,last + current)
+    if (last < 40) {
+        if (last == 0) {
+            println(last)
         }
+        println(current)
+        fibonacci(current, last + current)
     }
 
 }
-
